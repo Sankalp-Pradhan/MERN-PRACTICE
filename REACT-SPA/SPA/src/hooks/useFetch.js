24135b3c86815a8a76090f8a5d980894b28d9a39
Sankalp-Pradhan <sankalppradhan1906@gmail.com>
2025-04-24 12:22:56 +0530

    1

---
+++ REACT-SPA/SPA/src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import { use, useEffect, useState } from "react";
+
+//custom Hooks
+export function usePostTitle() {
+    const [post, setPost] = useState({});
+
+    async function getPosts() {
+        const respone = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+        const json = await respone.json();
+        setPost(json);
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, [])
+
+    return post.title;
+
+}
+
+export function useFetch(url) {
+    const [finalData, setFinalData] = useState({});
+    async function getDetails() {
+        const respone = await fetch(url);
+        const json = await respone.json();
+        setFinalData(json)
+    }
+
+    useEffect(() => {
+        getDetails()
+    }, [url]);
+
+    return {
+        finalData
+    }
+}
