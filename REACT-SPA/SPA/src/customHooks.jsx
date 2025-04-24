326282354ad0f1d2a77576ee99c306038ddc8eac
Sankalp-Pradhan <sankalppradhan1906@gmail.com>
2025-04-24 12:22:15 +0530

    useFetch hooks

--- REACT-SPA/SPA/src/customHooks.jsx
+++ REACT-SPA/SPA/src/customHooks.jsx
@@ -1,38 +1,21 @@
-import { useState } from "react";
-
-function useCounter() { 
-    const [count, setCount] = useState(0);
-    
-    function increaseCount() {
-        setCount(c => c + 1)
-    }
-    return {
-        count: count,
-        increaseCount: increaseCount
-    }
-}
+import { useEffect, useState } from "react";
+import { useFetch, usePostTitle } from "./hooks/useFetch";
 
 function App() {
-    return( <div>
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
+    const [currentPost, setCurrentPost] = useState(1);
+    const { finalData } = useFetch("https://jsonplaceholder.typicode.com/todos/" + currentPost);
 
-    </div>
+    return (
+        <div>
+            <button onClick={()=>setCurrentPost(1)}>1</button>
+            <button onClick={()=>setCurrentPost(2)}>2</button>
+            <button onClick={()=>setCurrentPost(3)}>3</button>
+            {JSON.stringify(finalData)}
+        </div >
     )
-}
-
-function Counter() {
-    const {count, increaseCount} = useCounter();
-
-    return <div>
-        <button onClick={increaseCount}>Increase{count}</button>
-    </div>
-
 
 }
+
 
 
 export default App
