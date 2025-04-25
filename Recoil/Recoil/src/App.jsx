10eb86de8ec5e2b68236228f0bd5f27f07b1c2bc
Sankalp-Pradhan <sankalppradhan1906@gmail.com>
2025-04-26 00:50:50 +0530

    recoil counter inrease decrease

---
+++ Recoil/Recoil/src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { counterAtom } from './store/atoms/counter'
+// import { useState } from 'react'
+import './App.css'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Counter />
+    </RecoilRoot>
+  )
+}
+function Counter() {
+
+  return <div>
+    <CurrentCount />
+    <Increase />
+    <Decrease />
+  </div>
+}
+
+function CurrentCount() {
+  const count = useRecoilValue(counterAtom);
+  return <div>
+    {count}
+  </div>
+}
+
+function Decrease() {
+  const setCount = useSetRecoilState(counterAtom)
+  function Decrease() {
+    setCount(c => c - 1);
+  }
+  return <div>
+    <button onClick={Decrease}>Decrease</button>
+  </div>
+}
+
+function Increase() {
+  const setCount = useSetRecoilState(counterAtom)
+  function Increase() {
+    setCount(c => c + 1);
+  }
+  return <div>
+    <button onClick={Increase}>Increase</button>
+  </div>
+}
+export default App
